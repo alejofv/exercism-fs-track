6cecd988f1bcf435fa33459e681c39e8d3d1508f
Alejandro Figueroa <alexphi@gmail.com>
2019-10-30 18:07:39 -0500

    Added new exercises

---
+++ accumulate/Accumulate.fs
@@ -0,0 +1,7 @@
+﻿module Accumulate
+
+let rec accumulate (func: 'a -> 'b) (input: 'a list): 'b list = 
+    // So, I can't use List.map...
+    match input with
+    | [] -> []
+    | first::other -> func first :: accumulate func other
---
+++ raindrops/Raindrops.fs
@@ -0,0 +1,15 @@
+﻿module Raindrops
+
+let convert (number: int): string = 
+    let factors = [(3,"Pling");(5,"Plang");(7,"Plong")]
+
+    let hasFactor n f = n % f = 0
+
+    let rainspeak =
+        factors
+        |> List.filter(fun (f,w) -> hasFactor number f)
+        |> List.map(fun (f,w) -> w)
+
+    match rainspeak with
+    | [] -> number.ToString()
+    | _ -> List.fold(+) "" rainspeak
