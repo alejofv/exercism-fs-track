7b0bb23ccca732a191235e8aa06775c84c2d6f66
Alejandro Figueroa <alexphi@gmail.com>
2019-11-07 15:16:31 -0500

    Added Bob and BeerSong exercises

---
+++ beer-song/BeerSong.fs
@@ -0,0 +1,32 @@
+﻿module BeerSong
+
+let count = function
+    | 0 -> "no more"
+    | n -> n.ToString()
+
+let descriptor = function
+    | 1 -> "bottle"
+    | _ -> "bottles"
+
+let sayBeers n =
+    sprintf "%s %s of beer" (count n) (descriptor n)
+
+let sayTakeDown = function
+    | 0 -> "Go to the store and buy some more"
+    | 1 -> "Take it down and pass it around"
+    | n -> "Take one down and pass it around"
+
+let capitalize (s :string) =
+    match s.[0] with
+    | 'n' -> "N" + s.[1..]
+    | _ -> s
+
+let sayVerse n = [
+    sprintf "%s on the wall, %s." (capitalize (sayBeers n)) (sayBeers n)
+    sprintf "%s, %s on the wall." (sayTakeDown n) (sayBeers (if n > 0 then n - 1 else 99))
+]
+
+let recite (startBottles: int) (takeDown: int) = 
+    [startBottles .. -1 .. (startBottles - takeDown + 1)]
+    |> List.map (sayVerse)
+    |> List.reduce (fun acc s -> acc @ [""] @ s)
---
+++ bob/Bob.fs
@@ -0,0 +1,17 @@
+﻿module Bob
+
+open System
+
+let response (input: string): string = 
+    let trimmed = input.Trim()
+
+    let empty = String.IsNullOrWhiteSpace(trimmed)
+    let question = trimmed.EndsWith("?")
+    let yell = Seq.exists Char.IsLetter trimmed && trimmed = trimmed.ToUpperInvariant()
+
+    if empty then "Fine. Be that way!"
+    elif yell then
+        if question then "Calm down, I know what I'm doing!"
+        else "Whoa, chill out!"
+    elif question then "Sure."
+    else "Whatever."
